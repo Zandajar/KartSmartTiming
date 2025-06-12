c1c3495e9fb99be6754512d90b681aec4ddf9364
zandajar <kovalev.and2004@gmail.com>
2025-06-12 06:32:24 +0300

    Write and testing first class for parsing

---
+++ Parsing.py
@@ -0,0 +1,31 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from Data_Classes.Driver import Driver
+
+
+def get_race_results():
+    driver = webdriver.Chrome()
+    driver.get("https://timing.batyrshin.name/tracks/narvskaya/heats/82924")
+
+    try:
+        WebDriverWait(driver, 10).until(
+            EC.presence_of_element_located((By.TAG_NAME, "table"))
+        )
+
+        tables = driver.find_elements(By.TAG_NAME, "table")
+
+        for i, table in enumerate(tables, 1):
+
+            rows = table.find_elements(By.TAG_NAME, "tr")
+
+            for row in rows:
+                cells = row.find_elements(By.XPATH, ".//th|.//td")
+                row_text = [cell.text.strip() for cell in cells]
+                return row_text
+
+    except Exception as e:
+        print(f"Ошибка: {e}")
+    finally:
+        driver.quit()
