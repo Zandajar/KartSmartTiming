4857d360d883fd705b6d13dfdf2980ce9c12812a
zandajar <kovalev.and2004@gmail.com>
2025-06-16 20:39:38 +0300

    Add parsing components

--- Parsing.py
+++ Parsing.py
@@ -5,8 +5,9 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 def get_race_results():
+    number = input("Write session id: ")
     driver = webdriver.Chrome()
-    driver.get(f"https://timing.batyrshin.name/tracks/narvskaya/heats/82924")
+    driver.get(f"https://timing.batyrshin.name/tracks/narvskaya/heats/{number}")
 
     try:
         # change in future
@@ -25,12 +26,34 @@ def get_race_results():
                 cells = row.find_elements(By.XPATH, ".//th|.//td")
                 row_text = [cell.text.strip() for cell in cells]
                 results.append(row_text)
-        return results
 
 
     except Exception as e:
         print(f"Ошибка: {e}")
     finally:
         driver.quit()
+    return results
 
-print(get_race_results())
+raw_data = get_race_results()
+print(raw_data)
+
+def find_subarray_index():
+    for index, subarray in enumerate(raw_data):
+        if "Gap" in subarray:
+            return index
+    return -1
+
+def get_driver_names():
+    return raw_data[1]
+
+def get_driver_place():
+    return get_driver_names().index(input("Write driver name: "))
+
+def get_kart_number():
+    return raw_data[2]
+
+def get_time_list():
+    return raw_data[3:find_subarray_index()]
+
+# test id - 82924
+print(get_time_list())
